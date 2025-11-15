c846e132dac9a1b0ff1e81e0ba2e1a7c01a7df16
Abdulahi Afolabi <afolabiabdulahi13@gmail.com>
2025-11-15 14:17:41 +0100

    Comment out unused import for load_breast_cancer
    Comment out the import statement for load_breast_cancer.

--- App.py
+++ App.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from sklearn.datasets import load_breast_cancer
+#from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.ensemble import RandomForestClassifier
